c17949c652965faf59de6890a1a26d84a7e0ebb4
abxcd111 <278704137@qq.com>
2021-01-18 20:02:48 +0800

    Add MergeSort

--- src/SortAlgorithm/SortAlgorithm.java
+++ src/SortAlgorithm/SortAlgorithm.java
@@ -36,14 +36,60 @@ public class SortAlgorithm {
         }
     }
 
-    public void SelectSort(int[] array){
+    //归并排序
+    public void MergeSort(int[] array){
+        MergeSort_t(array, 0, array.length - 1);
+    }
+    public void MergeSort_t(int[] array, int start, int end){
+        if(start == end)return;
 
+        int middle = (start + end)/2;
+
+        MergeSort_t(array, start, middle);
+        MergeSort_t(array, middle+1, end);
+        Merge(array, start, middle, end);
+    }
+    public void Merge(int[] array, int start, int middle, int end){
+        int length = end - start + 1;
+        int[] tempArray = new int[length];
+        int left = start, right = middle + 1;
+
+        for (int i = 0; i < length; i++){
+            if (left > middle){
+                for (int j = i; j < length; j++){
+                    tempArray[j] = array[right];
+                    right++;
+                }
+                break;
+            }
+            if (right > end){
+                for (int j = i; j < length; j++){
+                    tempArray[j] = array[left];
+                    left++;
+                }
+                break;
+            }
+
+            if (array[left] <= array[right]){
+                tempArray[i] = array[left];
+                left++;
+            }else {
+                tempArray[i] = array[right];
+                right++;
+            }
+        }
+
+        for (int i = 0; i < length; i++){
+            array[start+i] = tempArray[i];
+        }
     }
 
     public void SortTest(){
         int[] array = new int[] {4,7,3,7,3,6,3,7,4,6,6,8,2,3,4,5,8,9,4,6,3,7,8,1};
+
 //        BubbleSort(array);
-        InsertSort(array);
+//        InsertSort(array);
+        MergeSort(array);
         for (int i = 0; i < array.length; i++){
             System.out.print(" " + array[i]);
         }
